f7643f92407f191d8d7d2249d6fbccae18398272
n1k1ta28 <160599276+n1k1ta28@users.noreply.github.com>
2024-10-11 16:14:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,32 +13,29 @@ def mobile_to_desktop_tiktok(mobile_url):
         print(f"Response URL: {response.url}")  # Log the redirected URL
         if response.status_code == 200:
             redirected_url = response.url
-            match = re.search(r'@(.*?)\/(?:video|photo)\/(\d+)', redirected_url)
+            match = re.search(r'@(.*?)\/video\/(\d+)|\/video\/(\d+)', redirected_url)
             if match:
                 username = match.group(1)
-                video_id = match.group(2)
-                if 'video' in redirected_url:
-                    return f"https://www.tiktok.com/@{username}/video/{video_id}", "video"
-                elif 'photo' in redirected_url:
-                    return f"https://www.tiktok.com/@{username}/photo/{video_id}", "photo"
+                video_id = match.group(2) or match.group(3)
+                return f"https://www.tiktok.com/@{username}/video/{video_id}" if username else f"https://www.tiktok.com/video/{video_id}"
             else:
-                return None, None
+                return None
         else:
             print(f"Failed to fetch TikTok URL, Status Code: {response.status_code}, Response: {response.text}")  # Log failure
-            return None, None
+            return None
     except Exception as e:
         print(f"Error fetching URL: {e}")  # Print any errors encountered
-        return None, None
+        return None
 
 @app.route('/api/convert', methods=['POST'])
 def convert():
     data = request.json
     mobile_link = data.get('url')
     print("Mobile Link Received:", mobile_link)  # Debugging line
-    desktop_link, content_type = mobile_to_desktop_tiktok(mobile_link)
+    desktop_link = mobile_to_desktop_tiktok(mobile_link)
     
     if desktop_link:
-        return jsonify({'desktop_link': desktop_link, 'content_type': content_type})
+        return jsonify({'desktop_link': desktop_link})
     else:
         return jsonify({'error': 'Could not convert the mobile link.'}), 400
 
